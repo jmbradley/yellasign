9427c49eb808253813210067758879114d41e2d0
jmbradley <jmbradley808@gmail.com>
2018-12-01 02:13:04 -0500

    refactoring state

--- yellasigndotone/src/app/App.js
+++ yellasigndotone/src/app/App.js
@@ -1,63 +1,68 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 ///JB imports
-import luv from '../assets/atllove.png';
-import Form from '../form/Form';
-import Sign from '../sign/Sign';
-
+import luv from "../assets/atllove.png";
+import Form from "../form/Form";
+import Sign from "../sign/Sign";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       arrow: false,
-      inputValue: ''
-      };
-    }
-
-
-handleClick = () => {
-  console.log(this.state.arrow);
-
-    if(this.state.arrow === false) {
-
-      return this.setState({arrow:true});
-    }
-    else {
-      return this.setState({arrow:false});
-    }
+      inputValues: ["Your Text Here"],
+      inputValue: {
+        text: ""
+      }
+    };
   }
+
+  handleClick = () => {
+    console.log(this.state.arrow);
+
+    if (this.state.arrow === false) {
+      return this.setState({ arrow: true });
+    } else {
+      return this.setState({ arrow: false });
+    }
+  };
 
   handleChange = event => {
     event.preventDefault();
+    const inputValues = ["Your Text Here"];
+    const index = inputValues.indexOf([0]);
+    const newinputValue = event.target.value;
+    if (~index) {
+      inputValues[index] = newinputValue;
+    }
     this.setState({
-      inputValue: {
-        [event.target.name]: event.target.value,
-      }
-      
+      inputValue: newinputValue
+      // inputValues: { inputValues: [...this.state.inputValues, newinputValue] }
     });
   };
 
-
   render() {
-
-  console.log(this.state.inputValue);
+    console.log(this.state.inputValues);
     return (
       <div className="App">
-          <div className="mainLogo">
-            <h1 className="mainLogoH">YellaSign</h1>
-            <h3 className="mainLogoHHH">Film and Television Production Signs.</h3>
-          </div>
-            <img className="luv" src={luv} alt="Luv From ATL"/>
+        <div className="mainLogo">
+          <h1 className="mainLogoH">YellaSign</h1>
+          <h3 className="mainLogoHHH">Film and Television Production Signs.</h3>
+        </div>
+        <img className="luv" src={luv} alt="Luv From ATL" />
 
-          <div className="form-sign">
-              <Form handleClick={this.handleClick} arrow={this.state.arrow} handleChange={this.handleChange}/>
-            <div className="sign">
-              <Sign arrow={this.state.arrow} signText={this.state.inputValue}/>
-            </div>
+        <div className="form-sign">
+          <Form
+            handleClick={this.handleClick}
+            arrow={this.state.arrow}
+            handleChange={this.handleChange}
+          />
+          <div className="sign">
+            <Sign arrow={this.state.arrow} signText={this.state.inputValues} />
           </div>
         </div>
+      </div>
     );
   }
 }
--- yellasigndotone/src/sign/Sign.js
+++ yellasigndotone/src/sign/Sign.js
@@ -1,39 +1,39 @@
-import React from 'react';
-import OnePoint from '../assets/Black_Left_Arrow.png';
-import TwoPoint from '../assets/doublearrow.png';
-import luv from '../assets/atllove.png';
-import './Sign.css';
+import React from "react";
+import OnePoint from "../assets/Black_Left_Arrow.png";
+import TwoPoint from "../assets/doublearrow.png";
+import luv from "../assets/atllove.png";
+import "./Sign.css";
 
 function Sign(props) {
-    console.log(props.signText.signText);
+  console.log(props.signText);
 
-if (props.arrow === false) {
-    return ( 
-    <div>
+  if (props.arrow === false) {
+    return (
+      <div>
         <h4 className="dotheform"> Fill out the form to see your text</h4>
         <div className="signcard">
-            <img className="luvsign2" src={luv} alt="Luv From ATL"/>
-                <h1 className="top">{props.signText.signText} </h1>
-                    <img className="onePoint" src ={OnePoint} alt="Arrow"/>
-                <h1 className="bottom"> {props.signText.signText} </h1>
-            <img className="luvsign" src={luv} alt="Luv From ATL"/>
+          <img className="luvsign2" src={luv} alt="Luv From ATL" />
+          <h1 className="top">{props.signText} </h1>
+          <img className="onePoint" src={OnePoint} alt="Arrow" />
+          <h1 className="bottom"> {props.signText} </h1>
+          <img className="luvsign" src={luv} alt="Luv From ATL" />
         </div>
-    </div>
-    )}
-
-else {
-    return ( 
-        <div>
+      </div>
+    );
+  } else {
+    return (
+      <div>
         <h4 className="dotheform">Fill out form to see your text</h4>
         <div className="signcard1">
-            <img className="luvsign3" src={luv} alt="Luv From ATL"/>
-                <h1 className="top1">{props.signText.signText}</h1>
-                    <img className="img1" src ={TwoPoint} alt="Arrow"/>
-                <h1 className="bottom1"> {props.signText.signText} </h1>
-            <img className="luvsign1" src={luv} alt="Luv From ATL"/>
+          <img className="luvsign3" src={luv} alt="Luv From ATL" />
+          <h1 className="top1">{props.signText.signText}</h1>
+          <img className="img1" src={TwoPoint} alt="Arrow" />
+          <h1 className="bottom1"> {props.signText.signText} </h1>
+          <img className="luvsign1" src={luv} alt="Luv From ATL" />
         </div>
-        </div>
-    )}
+      </div>
+    );
+  }
 }
 
-  export default Sign;
+export default Sign;
